28509c49d854595508b23ae56a5e037f5aa16608
Vishnu Vardhan <vishnu.gujjula@aviso.com>
2025-04-19 22:33:28 +0530

    cors related changes & allowing multiple  origins

--- server/index.js
+++ server/index.js
@@ -11,11 +11,23 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.json({ limit: "10mb" }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://products-store-i7y8.onrender.com"
+];
+
 app.use(
   cors({
-      origin: "http://localhost:3000", // Your frontend URL
-      methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-      credentials: true, // Allow credentials (cookies, authorization headers)
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
@@ -23,12 +35,12 @@ app.use(cookieParser());
 app.use("/", productsRoute);
 
 if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, './../client/build')));
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, './../client/build')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.join(__dirname, './../client/build/index.html'))
-    );
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, './../client/build/index.html'))
+  );
 }
 
 
@@ -38,6 +50,6 @@ connectDB().then(() => {
     console.log(`Server running on http://localhost:${PORT}/`);
   });
 })
-.catch(() => {
-  console.log("Failed to start the server");
-});
+  .catch(() => {
+    console.log("Failed to start the server");
+  });
