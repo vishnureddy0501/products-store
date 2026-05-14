264f5ab4d599bca1a46a25b6b85cae0fddd3d767
Vishnu Vardhan <vishnu.gujjula@aviso.com>
2026-05-14 20:23:55 +0530

    improvements

---
+++ client/src/learning/sample.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+const Sample = () => {
+    useEffect(() => {
+
+        // this works as watcher also
+    }, []);
+    return (
+    <>
+        <div>Hello World</div>
+    
+    </>
+)}
+export default Sample;
